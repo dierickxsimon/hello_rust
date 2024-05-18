12c68d78f6c1f2e7982da60d5407efef6b3e543c
dierickxsimon <simon.dierickx10@gmail.com>
2024-05-18 23:44:03 +0200

    I'm a rustacean now

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#[allow(unused_variables)]
+fn main() {
+    let mut x: i32 = 5;
+
+    x += 1;
+
+    assert_eq!(x, 6);
+    println!("success");
+
+    let z: i32 = 10;
+    let y: i32 = 5;
+    {
+        let y: i32 = 10; // a variabel is only valid in this scope
+        println!("the value of z is {}, and y is {}", z, y);
+    }
+
+    println!("the value of z is {}, and y is {}", z, y);
+
+    define_x();
+
+    let y: i32 = 10;
+    //shadowing
+    let y: &str = "hello";
+    println!("the value of y is {}", y);
+
+    let (mut x, y) = (10, 20);
+    x +=1;
+    assert_eq!(x,11);
+    assert_eq!(y,20);
+
+    let (a, b);
+
+    (a, ..) = (3, 4); // .. we dont care
+    [.., b] = [1, 2]; // [] is array
+
+    assert_eq!([a,b],[3, 2])
+
+
+    
+}
+
+fn define_x(){
+    let x: &str = "hello";
+    println!("the value of x is {}", x);
+}
+
